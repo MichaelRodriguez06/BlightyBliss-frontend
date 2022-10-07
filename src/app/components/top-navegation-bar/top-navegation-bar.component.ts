d30d4ed4c3f154f74f4073e1e6ed9e08b517a9db
Michael <michael.rodriguez06@outlook.com>
2022-10-07 03:07:02 -0500

    formulario completo solo html y css hace falta terminar de arreglar estilos

---
+++ src/app/components/top-navegation-bar/top-navegation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-navegation-bar',
+  templateUrl: './top-navegation-bar.component.html',
+  styleUrls: ['./top-navegation-bar.component.css']
+})
+export class TopNavegationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
