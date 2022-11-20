32380cb0d4bdf26affaba17105b9f3b326d49572
Michael <michael.rodriguez06@outlook.com>
2022-11-20 11:38:10 -0500

    request get type documents into select html

---
+++ src/app/components/services/update-document.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../core/services/http/http.service';
+import { Observable } from 'rxjs';
+import { HttpApiResponse } from '../../core/models/http-api-response';
+import { TypeDocument } from '../../models/typeDocument';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateDocumentService {
+
+  url = `Access/login`;
+
+  constructor(private server: HttpService<TypeDocument[]>) {
+
+  }
+
+  getTypesDocument(): Observable<HttpApiResponse<TypeDocument[]>> {
+    return this.server.get(`FileType/false`);
+  }
+
+  updateDocument(formData: FormData) {
+    // this.server.ge
+  }
+
+}
