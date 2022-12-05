72ced77ceceeb35fbbaaebb44345ef82a455cc3a
DiegoOrtiz04 <Diego.ortiz04@uptc.edu.co>
2022-12-04 23:14:10 -0500

    Se rehace la option managens y se quita los convenios

--- src/app/components/agreement-crud/agreement-create.component.ts
+++ src/app/components/agreement-crud/agreement-create.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-agreement-crud',
   templateUrl: './agreement-crud.component.html',
-  styleUrls: ['./agreement-crud.component.css']
+  styleUrls: ['./agreement-crud.component.css',
+    '../../../../node_modules/primeflex/primeflex.css',
+    '../../../../node_modules/primeng/resources/themes/lara-light-indigo/theme.css']
 })
 export class AgreementCreateComponent implements OnInit {
+  agreementForm: FormGroup;
 
-  constructor() { }
+  constructor(private agreementFormfb: FormBuilder,) {
+    this.agreementForm = this.agreementFormfb.group({
+      agreements: this.agreementFormfb.array([this.agreementFormfb.group({agreement:['']})]),
+      companyAgreements: this.agreementFormfb.array([this.agreementFormfb.group({companyName:['']})])
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  get getAgreements(){
+    return this.agreementForm.get('agreements') as FormArray;
+  }
+
+  get getCompanyName(){
+    return this.agreementForm.get('companyAgreements') as FormArray;
+  }
+
+  addAgreements() {
+    const control = <FormArray>this.agreementForm.controls['agreements'];
+    control.push(this.agreementFormfb.group({agreement:[]}))
+  }
+
+  addCompanyName() {
+    const control = <FormArray>this.agreementForm.controls['companyAgreements'];
+    control.push(this.agreementFormfb.group({companyName:[]}))
+  }
+
+  removeAgreements(index: number){
+    const control = <FormArray>this.agreementForm.controls['agreements'];
+    control.removeAt(index)
+  }
+
+  removeCompanyName(index: number) {
+    const control = <FormArray>this.agreementForm.controls['companyAgreements'];
+    control.removeAt(index)
+  }
+
+  summitChangesAgreements() {
   }
 
 }
