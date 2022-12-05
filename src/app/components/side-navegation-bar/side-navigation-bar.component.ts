9ebd4249f2f4b3d22f64005707660e924c610088
Michael <michael.rodriguez06@outlook.com>
2022-12-05 12:35:14 -0500

    avance de crud de opciones

--- src/app/components/side-navegation-bar/side-navigation-bar.component.ts
+++ src/app/components/side-navegation-bar/side-navigation-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AppRoutes } from '../../core/services/app-routes';
+import {Component, OnInit} from '@angular/core';
+import {AppRoutes} from '../../core/services/app-routes';
 
 @Component({
   selector: 'app-side-navigation-bar',
@@ -49,7 +49,6 @@ export class SideNavigationBarComponent implements OnInit {
 
 
   constructor() {
-    console.log(this.items)
   }
 
   ngOnInit(): void {
