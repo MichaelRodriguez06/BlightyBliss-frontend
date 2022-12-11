9db559ae367b7496439cdf0bdb864a95235dfb5b
MichaelRodriguez06 <104533249+MichaelRodriguez06@users.noreply.github.com>
2022-12-11 15:26:11 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   hostUrl: "http://localhost:3006/",
-  apiUrl: "https://localhost:7187/api"
-  // apiUrl: "https://blightyapi.onrender.com/api"
+//   apiUrl: "https://localhost:7187/api"
+  apiUrl: "https://blightyapi.onrender.com/api"
 };
 
 /*
